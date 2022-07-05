2ed7d89fc77384b28e60661f6f9658e6d56ce845
Developer <support@tiniyo.com>
2022-07-05 08:52:32 +0530

    error template

--- errormap.go
+++ errormap.go
@@ -1,17 +1,40 @@
 package errors
 
+import (
+	"bytes"
+	"text/template"
+)
+
 type B2Code uint16
 
 // General Codes.
 const (
-	ResourceNotFound      B2Code = 20404
+	ResourceNotFound B2Code = 20404
 )
 
-var Templates = map[B2Code]ErrorResponse{
-  ResourceNotFound: {
-		Code: 20404,
-		Message: "The requested resource {{.Resource}} was not found",
+var ErrorTemplates = map[B2Code]ApiError{
+	ResourceNotFound: {
+		Code:     20404,
+		Message:  "The requested resource {{.Resource}} was not found",
 		MoreInfo: "https://www.twilio.com/docs/errors/20404",
-		Status: 404,
+		Status:   404,
 	},
+}
+
+func parseErrorField(tmplStr string, errInterface interface{}) (string, error) {
+	var err error
+	// with name passed as argument
+	validationTmpl := template.New("SipRtcValidation")
+	// "Parse" parses a string into a template
+	validationTmpl, err = validationTmpl.Parse(tmplStr)
+	if err != nil {
+		return tmplStr, err
+	}
+	var tmplBytes bytes.Buffer
+
+	if err = validationTmpl.Execute(&tmplBytes, errInterface); err != nil {
+		return tmplStr, err
+	}
+
+	return tmplBytes.String(), nil
 }
--- errors.go
+++ errors.go
@@ -1,51 +1,63 @@
 package errors
 
-
 type ApiError struct {
-  BaseUrl  string `json:"-"`
-	Code     int    `json:"code"`
-	Message  string `json:"message"`
-	MoreInfo string `json:"more_info"`
-	Status   int    `json:"status"`
+	Resource  string `json:"-"`
+	ErrorCode B2Code `json:"-"`
+	Code      int    `json:"code"`
+	Message   string `json:"message"`
+	MoreInfo  string `json:"more_info"`
+	Status    int    `json:"status"`
 }
 
-
-// Paginate handles the pagination calculation.
-func (ae *ApiError) Error() error {
+func (ae *ApiError) Generate() error {
 	ae.setDefaults()
 
-	if err := c.Message(); err != nil {
+	if err := ae.SetMessage(); err != nil {
 		return err
 	}
 
-	if err := c.doPaginate(); err != nil {
+	if err := ae.SetMoreInfo(); err != nil {
 		return err
 	}
 
-	if err := c.checkLinks(); err != nil {
+	if err := ae.SetStatus(); err != nil {
+		return err
+	}
+
+	if err := ae.SetCode(); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (ae *ApiError) Message() error {
-  if c.BaseUrl == "" {
-		return errors.New("BaseUrl value is missing")
-	}
-}
-// Sets the defaults values for current page
-// and limit if none of them were provided.
-func (ae *ApiError) setDefaults() {
-	if ae.Code == 0 {
-		ae.Code = 20404
-    ae.Status = 404
-	}
+
+func (ae *ApiError) SetMessage() error {
+	var err error
+	ae.Message, err = parseErrorField(ErrorTemplates[ae.ErrorCode].Message, ae)
+	return err
 }
 
-{
-    "code": 20404,
-    "message": "The requested resource /2010-04-01/Accounts/ACb83eb454cb3275d7b1200902935fed75/Conferences/1.json was not found",
-    "more_info": "https://www.twilio.com/docs/errors/20404",
-    "status": 404
+func (ae *ApiError) SetMoreInfo() error {
+	var err error
+	ae.Message, err = parseErrorField(ErrorTemplates[ae.ErrorCode].MoreInfo, ae)
+	return err
+}
+
+func (ae *ApiError) SetStatus() error {
+	var err error
+	ae.Status = ErrorTemplates[ae.ErrorCode].Status
+	return err
+}
+
+func (ae *ApiError) SetCode() error {
+	var err error
+	ae.Code = ErrorTemplates[ae.ErrorCode].Code
+	return err
+}
+
+func (ae *ApiError) setDefaults() {
+	if ae.ErrorCode == 0 {
+		ae.ErrorCode = ResourceNotFound
+	}
 }
